5312552b9f78266b10d5cf7e4bfb0bba9db14b09
mrelashvili <mrmrela5@gmail.com>
2022-10-31 02:02:25 +0400

    feat: add portfolio & skills sections

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BsTwitter, BsInstagram, BsLinkedin } from 'react-icons/bs';
+import { BsTwitter, BsLinkedin } from 'react-icons/bs';
 import { FaFacebookF } from 'react-icons/fa';
 
 const SocialMedia = () => {
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { motion } from 'framer-motion';
-import './Header.scss';
-import { images } from '../../constants';
 
 import { AppWrap } from '../../wrapper';
+import { images } from '../../constants';
+import './Header.scss';
 
 const scaleVariants = {
   whileInView: {
@@ -16,58 +16,56 @@ const scaleVariants = {
   },
 };
 
-const Header = () => {
-  return (
-    <div id="home" className="app__header app__flex">
-      <motion.div
-        whileInView={{ x: [-100, 0], opacity: [0, 1] }}
-        transition={{ duration: 1.5 }}
-        className="app__header-info"
-      >
-        <div className="app__header-badge">
-          <div className="badge-cmp app__flex">
-            <span>👋</span>
-            <div style={{ marginLeft: 20 }}>
-              <p className="p-text">Hello, I am</p>
-              <h1 className="head-text">Giorgi</h1>
-            </div>
-          </div>
-
-          <div className="tag-cmp app__flex">
-            <p className="p-text">Web Developer</p>
-            <p className="p-text">Freelancer</p>
+const Header = () => (
+  <div className="app__header app__flex">
+    <motion.div
+      whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+      transition={{ duration: 0.5 }}
+      className="app__header-info"
+    >
+      <div className="app__header-badge">
+        <div className="badge-cmp app__flex">
+          <span>👋</span>
+          <div style={{ marginLeft: 20 }}>
+            <p className="p-text">Hello, I am</p>
+            <h1 className="head-text">George</h1>
           </div>
         </div>
-      </motion.div>
 
-      <motion.div
-        whileInView={{ opacity: [0, 1] }}
-        transition={{ duration: 0.5, delayChildren: 0.5 }}
-        className="app__header-img"
-      >
-        <img src={images.profile} alt="profile_bg" />
-        <motion.img
-          whileInView={{ scale: [0, 1] }}
-          transition={{ duration: 1, ease: 'easeInOut' }}
-          src={images.circle}
-          alt="profile_circle"
-          className="overlay_circle"
-        />
-      </motion.div>
+        <div className="tag-cmp app__flex">
+          <p className="p-text">Web Developer</p>
+          <p className="p-text">Freelancer</p>
+        </div>
+      </div>
+    </motion.div>
 
-      <motion.div
-        variant={scaleVariants}
-        whileInView={scaleVariants.whileInView}
-        className="app__header-circles"
-      >
-        {[images.flutter, images.redux, images.sass].map((circle, idx) => (
-          <div className="circle-cmp app__flex" key={`circle-${idx}`}>
-            <img src={circle} alt="circle" />
-          </div>
-        ))}
-      </motion.div>
-    </div>
-  );
-};
+    <motion.div
+      whileInView={{ opacity: [0, 1] }}
+      transition={{ duration: 0.5, delayChildren: 0.5 }}
+      className="app__header-img"
+    >
+      <img src={images.profile} alt="profile_bg" />
+      <motion.img
+        whileInView={{ scale: [0, 1] }}
+        transition={{ duration: 1, ease: 'easeInOut' }}
+        src={images.circle}
+        alt="profile_circle"
+        className="overlay_circle"
+      />
+    </motion.div>
 
-export default Header;
+    <motion.div
+      variants={scaleVariants}
+      whileInView={scaleVariants.whileInView}
+      className="app__header-circles"
+    >
+      {[images.flutter, images.redux, images.sass].map((circle, index) => (
+        <div className="circle-cmp app__flex" key={`circle-${index}`}>
+          <img src={circle} alt="profile_bg" />
+        </div>
+      ))}
+    </motion.div>
+  </div>
+);
+
+export default AppWrap(Header, 'home');
